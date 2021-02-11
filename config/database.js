441ba3c47a8c5347b7bc497bf3bcb577378e9daa
teemu <teemu.hallinen@hotmail.com>
2021-02-11 20:03:00 +0200

    test

--- config/database.js
+++ config/database.js
@@ -11,7 +11,7 @@ require('dotenv').config()
 module.exports =  new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   protocol: 'postgres',
-  dialectOptions: {
-      ssl: true
+  ssl: {
+    rejectUnauthorized: false
   }
 })
