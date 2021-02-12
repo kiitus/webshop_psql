6bc5384e09c7a8fab3f1cbb40e55830c36afa42d
teemu <teemu.hallinen@hotmail.com>
2021-02-12 18:24:28 +0200

    conf_exp

--- config/database.js
+++ config/database.js
@@ -2,11 +2,19 @@ const { Sequelize, Model } = require('sequelize');
 
 require('dotenv').config()
 
+/*
 // Option 2: Passing parameters separately (other dialects)
 module.exports = new Sequelize(process.env.DATABASE,process.env.USER,process.env.PWORD, {
   host: 'localhost',
   dialect: 'postgres'
 });
+*/
 
+module.exports = new Sequelize(process.env.DATABASE_URL, {
+  logging: false,
+  dialectOptions: {
+    ssl: true /* for SSL config since Heroku gives you this out of the box */
+  }
+});
 
   
