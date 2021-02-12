4badffb3e6941805bfd1811a41420c2025858a15
teemu <teemu.hallinen@hotmail.com>
2021-02-12 18:21:21 +0200

    works_local

--- config/database.js
+++ config/database.js
@@ -3,29 +3,10 @@ const { Sequelize, Model } = require('sequelize');
 require('dotenv').config()
 
 // Option 2: Passing parameters separately (other dialects)
-/*module.exports = new Sequelize(process.env.DATABASE,process.env.USER,process.env.PWORD, {
+module.exports = new Sequelize(process.env.DATABASE,process.env.USER,process.env.PWORD, {
   host: 'localhost',
   dialect: 'postgres'
-});*/
+});
 
-/*module.exports =  new Sequelize("dflgha1dnv2mj0","svcjxbgyljqcfb","26a8034296e464ad6360e1c924e5ae62e92fd09ff2b60060f6e5e479ee86b01e",
-  {
-  host:"ec2-108-128-104-50.eu-west-1.compute.amazonaws.com",
-  dialect: 'postgres',
-  protocol: 'postgres',
-  ssl: { rejectUnauthorized: false }
-})*/
-
-/*module.exports =  new Sequelize(process.env.DATABASE_URL,
-  {
-  dialect: 'postgres',
-  protocol: 'postgres',
-  //ssl: { rejectUnauthorized: false }})
-  }) */
-
-  sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres'
-  });
-  
 
   
